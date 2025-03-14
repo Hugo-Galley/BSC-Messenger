23582e8f5c692a9917017fcdf3701cbeb2b5648f
Freaks <galleyhugo@icloud.com>
2025-03-14 14:13:02 +0100

    Ajout de la clé primaire dans un fichier sur le PC + Création README.MD

--- Class/User.py
+++ Class/User.py
@@ -2,7 +2,7 @@ import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-from os import urandom
+import os
 
 
 class User:
@@ -12,7 +12,7 @@ class User:
         self.password = password
         self.publicKey = ""
         self.privateKey = ""
-        self.salt = base64.b64encode(urandom(16)).decode('utf-8')
+        self.salt = base64.b64encode(os.urandom(16)).decode('utf-8')
 
     def CreateKeyPair(self):
         self.privateKey = rsa.generate_private_key(
@@ -20,6 +20,12 @@ class User:
             key_size=2048,
         )
         self.publicKey = self.privateKey.public_key()
+        os.makedirs(os.path.join(os.getcwd(), "clients", self.username), exist_ok=True)
+        self.GetkeyPairInTextFormat()
+        with open(os.path.join("clients", self.username, "privateKey.key"), mode="w+") as f:
+            f.write(self.privateKey)
+
+
 
     def GetkeyPairInTextFormat(self):
         self.privateKey = self.privateKey.private_bytes(
