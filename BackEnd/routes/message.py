c0edc63f927aee2eafa919b270983e56c937e0ca
Freaks <galleyhugo@icloud.com>
2025-03-31 12:14:56 +0200

    Clean du code + ajout de la correction du Pop Up

--- BackEnd/routes/message.py
+++
@@ -1,8 +0,0 @@
-from config import db
-from fastapi import APIRouter
-
-router = APIRouter()
-
-# @router.get("/message/decrypt")
-# async def get_decrypt_message(decrypt_message : DecryptMessage):
-#     pass
