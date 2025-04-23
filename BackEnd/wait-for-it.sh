47171594762b0d3785b453fef100836e77cf2131
Your Name <you@example.com>
2025-04-23 10:24:46 +0200

    PAssage en LF, pour faire foncionner la méthode 1 sur Windows, (Bug trouvé apres avoir testé sur Windows (seulement si besoin de la méthode 1)

--- BackEnd/wait-for-it.sh
+++ BackEnd/wait-for-it.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
+# Use this script to test if a given TCP host/port are availablee
+
 
 WAITFORIT_cmdname=${0##*/}
 
